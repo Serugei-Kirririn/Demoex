b9d629739dcf55ea455ab0a3888d819c94676b25
BeforeDice <podisa0135@gmail.com>
2020-12-11 14:28:24 +0900

    map

--- app/src/main/java/com/example/demoekz/MapsActivity.java
+++ app/src/main/java/com/example/demoekz/MapsActivity.java
@@ -8,6 +8,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,8 +41,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng yakutsk = new LatLng(62, 129);
+        mMap.addMarker(new MarkerOptions().position(yakutsk).title("Marker in Yakutsk").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(yakutsk));
     }
 }
--- app/src/main/java/com/example/demoekz/StartScreen.java
+++ app/src/main/java/com/example/demoekz/StartScreen.java
@@ -7,16 +7,37 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StartScreen extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class StartScreen extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.startscreen);
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
     }
     public void onClickMenu(View v) {
         Intent a = new Intent(StartScreen.this, Menu.class);
         startActivity(a);
         finish();
+    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng yakutsk = new LatLng(62.03380, 129.73306);
+        mMap.addMarker(new MarkerOptions().position(yakutsk).title("Marker in Yakutsk").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(yakutsk));
     }
 }
